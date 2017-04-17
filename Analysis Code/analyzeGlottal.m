46f532ae9ec8a17de32403440e23d3421769464b
nrk7 <nitish.katta@gmail.com>
2017-04-17 21:36:27 +0200

    updated analyzeglottal

--- Analysis Code/analyzeGlottal.m
+++ Analysis Code/analyzeGlottal.m
@@ -1,16 +1,16 @@
-function [g] = analyzeGlottal(fileName, start, ending, ldft)
+function [g] = analyzeGlottal(fileName, start, ending)
 %% analyzeGlottal - uses an interval of a larger waveform to estimate the 
 % glottal waveform. A combination of lpcacm.m and glottalWav.m 
  % inputs:
   % fileName - string, input waveform file name
   % start - number, target beginning in seconds
   % ending - number, target ending in seconds
-  % ldft - length of the dft to be used
- % outputs
   % g - glottal waveform
   
+ 
 % read initial audio
- [s, fs] = audioread(fileName);
+[s, fs] = audioread(fileName);
+
 % s is the input waveform segment, zero outside the interval 0, N - 1
 % sn(m) = s(m + n)w(m) - use a hamming window as it reduces error and the
 % book recommends it
@@ -22,7 +22,8 @@ sn = s(n:(n + m - 1)).*hamming(m); % windowing selected segment
  % 44 from 44 kHz sampling rate and 4 from source excitation spectrum and
  % radiation load
 % p = round(fs/1000) + 4;
- p = 15;
+% p-value that isn't overkill:
+p = 64;
 
 % calculating the short-time autocorrelation function Rn(k)
 Rn = zeros(p + 1, 1); % plus one because no zero indexing
@@ -43,16 +44,17 @@ ak = Rt\Rn(2:end);
  for k = 1:length(ak)
      akRn = akRn + ak(k)*Rn(k + 1);
  end
- gain = sqrt(Rn(1) - akRn); % Calculation of G
+gain = sqrt(Rn(1) - akRn); % Calculation of gain
 
 % A(k) = 1 - sum(ak*z^-k)
 % obtaining formant dft
+ldft = floor((ending - start)*fs); % make length as long as possible
 A = zeros(ldft, 1);
-for z = 1:length(A)
-    for k = 1:length(ak)
-        A(z) = A(z) + ak(k)*(exp(2j*pi/length(A)*(z - 1)))^(1 - k); 
+for k = 1:length(A)
+    for n = 1:length(ak)
+        A(k) = A(k) + ak(n)*(exp(-2j*pi*(k - 1)*(n - 1)/length(A))); 
     end
-    A(z) = 1 - A(z);
+    A(k) = 1 - A(k);
 end
  
 % V(z) = G/A(z)
@@ -68,11 +70,11 @@ end
 % G(z) = Sn(z)/V(z)
 G = Sn./V;
 
-% g(n) = idft(G(z))
+% g(n) = ifft(G(z))
 g = zeros(length(G), 1);
 for n = 1:length(g)
     for k = 1:length(G)
-        g(n) = g(n) + G(k)*exp(2j*pi/length(G)*(k - 1)*(n - 1));
+        g(n) = g(n) + G(k)*exp(2j*pi*(k - 1)*(n - 1)/length(G));
     end
     g(n) = g(n)/length(G);
 end
@@ -80,24 +82,36 @@ end
 % plots ffts of the waveforms
 figure;
 subplot(2,2,1);
-plot(abs(A));
+plot(fs/length(A)*(0:(length(A(1:round(end/2))) - 1)), abs(A(1:round(end/2))));
 title('|A(k)|');
+xlabel('frequency (Hz)');
+ylabel('magnitude');
 subplot(2,2,2);
-plot(abs(V));
+plot(fs/length(V)*(0:(length(V(1:round(end/2))) - 1)), abs(V(1:round(end/2))));
 title('|V(k)|')
+xlabel('frequency (Hz)');
+ylabel('magnitude');
 subplot(2,2,3);
-plot(abs(Sn));
+plot(fs/length(Sn)*(0:(length(Sn(1:round(end/2))) - 1)), abs(Sn(1:round(end/2))));
 title('|Sn(k)|');
+xlabel('frequency (Hz)');
+ylabel('magnitude');
 subplot(2,2,4);
-plot(abs(G));
+plot(fs/length(G)*(0:(length(G(1:round(end/2))) - 1)), abs(G(1:round(end/2))));
 title('|G(k)|');
+xlabel('frequency (Hz)');
+ylabel('magnitude');
 
 % plots glottal waveform
 figure;
 subplot(2,1,1);
 plot(0:1/fs:(length(g) - 1)/fs, abs(g));
 title('|g(n)|');
+xlabel('time  (s)');
+ylabel('magnitude');
 subplot(2,1,2);
 plot(0:1/fs:(length(sn) - 1)/fs, abs(sn));
 title('|sn(n)|');
+xlabel('time (s)');
+ylabel('magnitude');
 end
